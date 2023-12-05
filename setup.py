a0476a70cb6bbfd0c050d82ee3dcdb79fcc6df89
fsossai <federico.sossai@gmail.com>
2023-12-05 02:26:26 -0600

    Added description to `setup.py`

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="fslog",
     version="0.1.0",
+    description="Customizable log formatter that supports recursive log sections",
     packages=find_packages(),
     install_requires=[]
 )
